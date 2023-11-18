13420f01b1a5728c7d871446bedc3a251d9390f4
Nevitonbastos <147950585+Nevitonbastos@users.noreply.github.com>
2023-11-17 21:31:18 -0300

    novo

---
+++ ex012-2.js
@@ -0,0 +1,7 @@
+var vel = 100
+console.log(`A velocidade do seu carro está em ${vel} km/h`)
+if( vel > 60 ){
+    console.log('Você esta acima da velicodade permitida, Multado!')
+}else{
+    console.log('Você esta na velocidade correta!')
+}
